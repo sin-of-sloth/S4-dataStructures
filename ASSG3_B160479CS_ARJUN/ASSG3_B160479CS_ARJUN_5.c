403ac4610b1a257702b6dae47dd2b421fe295e4b
Arjun Lal <thedarklordisrising@gmail.com>
2018-02-19 04:13:42 +0530

    Living upto my username.

--- ASSG3_B160479CS_ARJUN/ASSG3_B160479CS_ARJUN_5.c
+++ ASSG3_B160479CS_ARJUN/ASSG3_B160479CS_ARJUN_5.c
@@ -1,7 +1,9 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-long int S[10000];
+#define MAX 100000
+
+long int S[MAX];
 int top = -1;
 
 void push(long int a)
@@ -17,91 +19,71 @@ long int pop()
 
 int main()
 {
-	char *postfix = (char*)malloc(sizeof(char));
-	int symbol;
-	long int length = 0, i, result, num1, num2;
-	
-	//INPUTTING THE postfix EXPRESSION
-	do{
-		length++;
-		symbol = getchar();
-		postfix = (char*)realloc(postfix, sizeof(char) * length);
-		postfix[length-1] = symbol;
-		
-		if(symbol>='0' && symbol<='9')
-		{
-			if(length>0 && postfix[length-2]!=' ')
-			{
-				num1 = symbol-48 + 10*pop();
-				push(num1);
-			}
-			else
-				push(symbol-48);
-		}
-		else if(symbol == ' ')
-			continue;
-		else if(symbol == '\n')
-			break;
-		else
-		{
-			num2 = pop();
-			num1 = pop();
-			if(symbol == '+')
-				push(num1 + num2);
-			else if(symbol == '-')
-				push(num1-num2);
-			else if(symbol == '*')
-				push(num1*num2);
-			else if(symbol == '/')
-				push(num1/num2);
-			else if(symbol == '%')
-				push(num1%num2);
-		}
-	}while(symbol != '\n');
-	result = pop();
-	printf("%ld\n%s", result, postfix);
-	char infix[length];
-	
-	//BUILDING infix EXPRESSION
-	
-	
-	//EVALUATING THE RESULT
-	for(i=0;i<length-1;i++)
+	int infix[MAX];
+	char *postfix = (char*)malloc(sizeof(char)), ch;
+	int i, j, inLen = -1, postLen = -1;
+	long int a = 0, res, num1, num2;
+	for(i=0;i<MAX;i++)
+		infix[i] = -1;
+	while((ch = getchar()) != '\n')
 	{
-		if(postfix[i]>='0' && postfix[i]<='9')
+		postLen+=1;
+		postfix = (char*)realloc(postfix, sizeof(char) * (postLen + 1));
+		postfix[postLen] = ch;
+	}
+	
+	i=0;
+	for(i=0; i<=postLen; i++)
+	{
+		if(postfix[i] == ' ')
 		{
-			if(i>0 && postfix[i-1]>='0' && postfix[i-1]<='9')
+			if((i>0) && (postfix[i-1]>='0') && (postfix[i-1]<='9'))
 			{
-				num1 = (int)postfix[i];
-				num1 = num1-48 + (10*pop());
-				push(num1);
-			}
-			else
-			{
-				num1 = (int)postfix[i];
-				push(num1-48);
+				push(a);
+				infix[++inLen] = a;
+				a=0;
+				inLen++;
 			}
 		}
-		else if(postfix[i] == ' ')
-			continue;
-		else if(postfix[i] == '\n')
-			break;
+		else if((postfix[i]>='0') && (postfix[i]<='9'))
+		{
+			a = a*10 + (int)postfix[i] - 48;
+		}
 		else
 		{
+			j = inLen-2;
+			while(infix[j] != -1)
+				j-=2;
+			infix[j] = (int)postfix[i];
 			num2 = pop();
 			num1 = pop();
 			if(postfix[i] == '+')
 				push(num1 + num2);
 			else if(postfix[i] == '-')
-				push(num1-num2);
+				push(num1 - num2);
 			else if(postfix[i] == '*')
-				push(num1*num2);
+				push(num1 * num2);
 			else if(postfix[i] == '/')
 				push(num1/num2);
 			else if(postfix[i] == '%')
-				push(num1%num2);	
+				push(num1%num2);
+			else if(postfix[i] == '^')
+			{
+				a=num1;
+				for(j=0;j<num2-1;j++)
+					num1 = num1 * a;
+				push(num1);
+			}
 		}
 	}
-
+	res = pop();
+	for(i=0;i<=inLen;i++)
+	{
+		if(i%2 == 0)
+			printf("%d", infix[i]);
+		else
+			printf("%c", infix[i]);
+	}
+	printf("\n%ld", res);
 	return 0;
 }
